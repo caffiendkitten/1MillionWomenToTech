3556a38819d3b8031063d062df1e77afc5cf8a38
thatonechick1987 <caffiendkitten@gmail.com>
2018-07-18 20:54:09 -0700

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,14 @@
+# general calculations
+print("Some general calclations to play with...")
+print (1+1)
+print (3)
+print ( 5 * (12-8) + -15)
+print (2<<9)
+print (2**9)
+print("")
+print ("Some calculations for day of birth...")
+print ("hours in a year: ", 365*24)
+print("minutes in a decade: ", 10*365*24*60)
+print ("age in seconds: ", 30*365*24*60*60)
+print("andreea's age is", ((((48618000/365)/24)/60/60)*10))
+
